9f14ca680b92af6ea5447daf15b55c7d6dc80bdd
Endryus Schmidel <endryusschmidel@gmail.com>
2026-05-14 10:19:35 -0300

    feat(security): replace hardcoded telegram token with environment variables
    I removed the API key from my Telegram bot. Now, if you want to use it, you'll need to add its key to your IDE's environment variables. (More security)

--- back-end/src/main/java/com/example/schmidel/projeto_hospital/services/TelegramService.java
+++ back-end/src/main/java/com/example/schmidel/projeto_hospital/services/TelegramService.java
@@ -1,5 +1,6 @@
 package com.example.schmidel.projeto_hospital.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -7,11 +8,14 @@ import org.springframework.web.client.RestTemplate;
 public class TelegramService {
 
     // 🚨 SUBSTITUA PELOS SEUS DADOS REAIS
-    private final String BOT_TOKEN = "8612177012:AAFFPUYcpyVghHBJjAn1UiOKVYoBQAQwaok";
-    private final String CHAT_ID = "7193248811";
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Value("${telegram.chat.id}")
+    private String chatId;
 
     public void sendAlert(String message) {
-        String url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=" + message;
+        String url = "https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + chatId + "&text=" + message;
 
         try {
             RestTemplate restTemplate = new RestTemplate();
