2e547400559e6a7397d1574b246eafd160e85c47
Endryus Schmidel <endryusschmidel@gmail.com>
2026-05-16 02:00:13 -0300

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login/>}/>
 
-          <Route path='/register' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
 
           <Route path='/dashboard' element={<Dashboard/>}/>
 
